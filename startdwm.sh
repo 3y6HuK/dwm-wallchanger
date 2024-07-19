fb84c8e18edda27a3211cd9b501277b217722a3a
3y6HuK <freedom_sky@bk.ru>
2024-07-19 21:27:04 +1000

    Update startdwm.sh

--- startdwm.sh
+++ startdwm.sh
@@ -1,14 +1,13 @@
 #!/bin/sh
 
-#wal -R & 
 $HOME/.dwm/dwmscripts/walls.sh & 
 picom --animations --animation-window-mass 0.5 --animation-for-open-window zoom --animation-stiffness-tag-change 500 &
-slstatus &
 twmnd &
+slstatus &
+#dwmblocks &
 setxkbmap us,ru -option 'grp:alt_shift_toggle' &
 #setxkbmap -model pc105 -layout us,ru -option grp:alt_shift_toggle &
 #setxkbmap us,ru -option 'grp:caps_toggle' &
-#dwmblocks &
 export XCURSOR_SIZE=32 # default is 24
 #exec /usr/libexec/polkit-gnome-authentication-agent-1 & # for Gentoo
 exec /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
