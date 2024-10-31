87760e576e3d2e74be44694ccd16f2dfec42b3ce
3y6HuK <freedom_sky@bk.ru>
2024-10-31 21:15:38 +1000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd ~/ && git clone https://aur.archlinux.org/yay-bin.git && cd yay-bin && makepkg -si
+cd ~/ && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si
 
 yay -Sy xorg-server xorg-xinit xorg-drivers python-pywal python-pywalfox xdotool kitty feh flameshot atool imagemagick perl-image-exiftool ueberzug alsa-utils dmenu rofi rofi-themes-collection-git xorg-xsetroot twmn-git ranger pipewire pipewire-alsa pipewire-jack pipewire-pulse pavucontrol pcmanfm numlockx betterlockscreen ttf-jetbrains-mono-nerd ttf-unifont ttf-liberation ttf-dejavu libev uthash libconfig telegram-desktop obs-studio thunderbird libxinerama ttf-nerd-fonts-symbols cppcheck neovim meson ninja picom-simpleanims-netx-git whatsapp-for-linux polkit-gnome tokyonight-gtk-theme-git kora-icon-theme nwg-look zsh zsh-syntax-highlighting zsh-autosuggestions  oh-my-zsh-git zsh-fast-syntax-highlighting-git imlib2 npm make cmake btop gamemode icoutils firefox firefox-adblock-plus firefox-i18n-ru discord cherrytree paccache-hook grub-hook --noconfirm
 wget https://mega.nz/linux/repo/Arch_Extra/x86_64/megasync-x86_64.pkg.tar.zst && sudo pacman -U "$PWD/megasync-x86_64.pkg.tar.zst"
